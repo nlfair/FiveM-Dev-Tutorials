19b4212bef6a4019b8c411f325022305a2ddd5dc
nlfair <68623429+nlfair@users.noreply.github.com>
2025-06-02 16:48:30 -0400

    Episode 2 changes

---
+++ resources/[tutorials]/nf_spawn/client/client.lua
@@ -0,0 +1,21 @@
+--spawnPos = vector3(686.245, 577.950, 130.461)
+spawnPos = vector3(-275.522, 6635.835, 7.425)
+
+AddEventHandler('onClientGameTypeStart', function()
+    exports.spawnmanager:setAutoSpawnCallback(function()
+        exports.spawnmanager:spawnPlayer({
+            x = spawnPos.x,
+            y = spawnPos.y,
+            z = spawnPos.z,
+            --model = 'a_m_m_skater_01'
+            model = 'a_c_chimp'
+        }, function()
+            TriggerEvent('chat:addMessage', {
+                args = { 'Welcome to the party!~' }
+            })
+        end)
+    end)
+
+    exports.spawnmanager:setAutoSpawn(true)
+    exports.spawnmanager:forceRespawn()
+end)
---
+++ resources/[tutorials]/nf_spawn/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Norman Fair'
+description 'Description goes here'
+version '0.1.0'
+
+resource_type 'gametype' {name = 'Tutorial Type!'}
+
+client_scripts {
+    'client/client.lua',
+}
