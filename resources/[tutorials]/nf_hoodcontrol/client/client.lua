3dfc71850deed353af8a99b964a8c76cb3201080
nlfair <68623429+nlfair@users.noreply.github.com>
2025-06-08 18:41:19 -0400

    Tutorial 5, made opening and closing doors mappable commands and added rolling up and down the drivers window

--- resources/[tutorials]/nf_hoodcontrol/client/client.lua
+++ resources/[tutorials]/nf_hoodcontrol/client/client.lua
@@ -1,28 +1,80 @@
-CreateThread(function()
-    while true do
-        Wait(0)
+RegisterCommand('+openhood', function()
+    local playerId = PlayerPedId()
+    local playerVehicle = GetVehiclePedIsIn(playerId, false)
 
-        local playerId = PlayerPedId()
-        local playerVehicle = GetVehiclePedIsIn(playerId, false)
-
-        if playerVehicle > 0 and GetPedInVehicleSeat(playerVehicle, -1) == playerId then
-            -- Hood (doesn't work with Panto)
-            if IsControlJustReleased(0, 208) then
-                if GetVehicleDoorAngleRatio(playerVehicle, 4) > 0.1 then
-                    SetVehicleDoorShut(playerVehicle, 4, false)
-                else
-                    SetVehicleDoorOpen(playerVehicle, 4, false, false)
-                end
-            end
-
-            -- Trunk
-            if IsControlJustReleased(0, 207) then
-                if GetVehicleDoorAngleRatio(playerVehicle, 5) > 0.1 then
-                    SetVehicleDoorShut(playerVehicle, 5, false)
-                else
-                    SetVehicleDoorOpen(playerVehicle, 5, false, false)
-                end
-            end
-        end
+    if playerVehicle <= 0 then
+        return
     end
-end)
+
+    if GetPedInVehicleSeat(playerVehicle, -1) ~= playerId then
+        return
+    end
+
+    if GetVehicleDoorAngleRatio(playerVehicle, 4) > 0.1 then
+        SetVehicleDoorShut(playerVehicle, 4, false)
+    else
+        SetVehicleDoorOpen(playerVehicle, 4, false, false)
+    end
+end, false)
+
+
+
+RegisterCommand('+opentrunk', function()
+    local playerId = PlayerPedId()
+    local playerVehicle = GetVehiclePedIsIn(playerId, false)
+
+    if playerVehicle <= 0 then
+        return
+    end
+
+    if GetPedInVehicleSeat(playerVehicle, -1) ~= playerId then
+        return
+    end
+
+    if GetVehicleDoorAngleRatio(playerVehicle, 5) > 0.1 then
+        SetVehicleDoorShut(playerVehicle, 5, false)
+    else
+        SetVehicleDoorOpen(playerVehicle, 5, false, false)
+    end
+end, false)
+
+
+
+RegisterCommand('+rollupwindow', function()
+    local playerId = PlayerPedId()
+    local playerVehicle = GetVehiclePedIsIn(playerId, false)
+
+    if playerVehicle <= 0 then
+        return
+    end
+
+    if GetPedInVehicleSeat(playerVehicle, -1) ~= playerId then
+        return
+    end
+
+    RollUpWindow(playerVehicle, 0)
+end, false)
+
+
+
+RegisterCommand('+rolldownwindow', function()
+    local playerId = PlayerPedId()
+    local playerVehicle = GetVehiclePedIsIn(playerId, false)
+
+    if playerVehicle <= 0 then
+        return
+    end
+
+    if GetPedInVehicleSeat(playerVehicle, -1) ~= playerId then
+        return
+    end
+
+    RollDownWindow(playerVehicle, 0)
+end, false)
+
+
+
+RegisterKeyMapping('+openhood', 'Open Vehicle Hood', 'keyboard', 'PAGEUP')
+RegisterKeyMapping('+opentrunk', 'Open Vehicle Trunk', 'keyboard', 'PAGEDOWN')
+RegisterKeyMapping('+rollupwindow', 'Roll Up Vehicle Window', 'keyboard', 'HOME')
+RegisterKeyMapping('+rolldownwindow', 'Roll Down Vehicle Window', 'keyboard', 'END')
